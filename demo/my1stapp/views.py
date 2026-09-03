a212b9c657dac2e9dd8dcb8db774c51d9a479127
Anthony Leonard <leonard.ant@gmail.com>
2026-09-03 12:47:01 +0100

    added todo, and config in DB

--- demo/my1stapp/views.py
+++ demo/my1stapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render, HttpResponse
 def home(request):
     return render(request, "home.html", {"a_variable": "Hello World! This is my first Django app."})
         ##HttpResponse("Hello World! This is my first Django app.")    
+
+
+def todos(request):
+    from .models import ToDoItem
+    todos = ToDoItem.objects.all()
+    return render(request, "todos.html", {"todos": todos})
