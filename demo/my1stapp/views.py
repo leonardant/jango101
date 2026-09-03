9d6ac941687a26a8d53004aa0ee69c19b58d9cd2
Anthony Leonard <leonard.ant@gmail.com>
2026-09-03 17:27:59 +0100

    errors, alerts etc...

--- demo/my1stapp/views.py
+++ demo/my1stapp/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, HttpResponse
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
