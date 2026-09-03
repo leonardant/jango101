e0840149331f00eebea87e9623510b6fa573f6e0
Anthony Leonard <leonard.ant@gmail.com>
2026-09-03 16:00:43 +0100

    relocate files and change refs accordingly

--- demo/my1stapp/views.py
+++ demo/my1stapp/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html", {"a_variable": "Hello World! This is my first Django app."})
+    return render(request, "my1stapp/home.html", {"a_variable": "Hello World! This is my first Django app."})
         ##HttpResponse("Hello World! This is my first Django app.")    
 
 
 def todos(request):
     from .models import ToDoItem
     todos = ToDoItem.objects.all()
-    return render(request, "todos.html", {"todos": todos})
+    return render(request, "my1stapp/todos.html", {"todos": todos})
