b2ed8aab1d5240d933cfdc617e92e94c5b170662
Anthony Leonard <leonard.ant@gmail.com>
2026-09-03 21:56:34 +0100

    enhanced todo items and did migrations

--- demo/my1stapp/models.py
+++ demo/my1stapp/models.py
@@ -1,7 +1,32 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
 class ToDoItem(models.Model):
-    title = models.CharField(max_length=100)
+
+    title = models.CharField(
+        max_length=100,
+    )
+
     description = models.TextField()
-    completed = models.BooleanField(default=False)
+
+    completed = models.BooleanField(
+        default=False,
+    )
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="todo_items",
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+    def __str__(self):
+        return self.title
