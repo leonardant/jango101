fb1729bed90445f37605d6593009de1d34fa8742
Anthony Leonard <leonard.ant@gmail.com>
2026-09-03 17:21:29 +0100

    changes for Icons

---
+++ demo/my1stapp/templatetags/icons.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag(
+    "components/icon.html"
+)
+def icon(name, size=24):
+    return {
+        "name": name,
+        "size": size,
+    }
