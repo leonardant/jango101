38ce37892d5f7d6e8db67dd30e252771ee4bf89f
Anthony Leonard <leonard.ant@gmail.com>
2026-09-04 22:14:02 +0100

    code review - point 1

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -9,19 +9,30 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
+import os
+
+from dotenv import load_dotenv
+
+
+
 
 from pathlib import Path
 from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+## BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+load_dotenv(
+    BASE_DIR / ".env"
+)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9q8ur=*zb&=pica1i1)o2pbc8d-%2f+bsnjxwwmq0s13(%#9z5'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
