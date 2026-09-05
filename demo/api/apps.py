075e6e850539f1fe2ac5fdd57de9fe2fa02bcbbf
Anthony Leonard <leonard.ant@gmail.com>
2026-09-05 10:45:03 +0100

    ruff #4 with exceptions for custom lines!

--- demo/api/apps.py
+++ demo/api/apps.py
@@ -8,4 +8,4 @@ class ApiConfig(AppConfig):
 
     def ready(self):
 
-        import api.signals
+        import api.signals # noqa: F401
--- demo/my1stapp/apps.py
+++ demo/my1stapp/apps.py
@@ -10,4 +10,4 @@ class My1stappConfig(AppConfig):
 
     def ready(self):
 
-        import my1stapp.signals
+        import my1stapp.signals # noqa: F401
