c8038ef965d5f761af22251aa67c0ac48999d6be
Anthony Leonard <leonard.ant@gmail.com>
2026-09-05 21:52:14 +0100

    set coverage threshold to 55% and exclude some folders

--- scripts/check.sh
+++ scripts/check.sh
@@ -218,7 +218,7 @@ uv run coverage run \
 
 section "Coverage report"
 
-uv run coverage report -m
+uv run coverage report -m --fail-under=55
 
 
 # ============================================================
